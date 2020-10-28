a3e24a268e907424856061362d491cc6459c9d28
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-10-27 22:08:27 -0300

    01 Default Methods

---
+++ src/br/com/alura/java8/OrdenaStrings.java
@@ -0,0 +1,61 @@
+package br.com.alura.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+
+		// Modo "velho"
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+
+		Comparator<String> comparador = new ComparadorPorTamanho();
+
+		// Antes do Java 8
+//		Collections.sort(palavras, comparador);
+
+		// Após o Java 8
+		palavras.sort(comparador);
+
+		System.out.println(palavras);
+
+//		for (String p : palavras) {
+//			System.out.println(p);
+//		}
+
+		Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
+
+	}
+
+}
+
+class ImprimeNaLinha implements Consumer<String> {
+
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+	}
+
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return -1;
+		}
+		if (s1.length() > s2.length()) {
+			return 1;
+		}
+		return 0;
+	}
+
+}
